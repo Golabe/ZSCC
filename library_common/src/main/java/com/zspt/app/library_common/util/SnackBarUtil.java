1f70401cd16871b008eed8420b34031a5e98d971
yuequan <2643552374@qq.com>
2017-10-09 11:02:19 +0800

    更新

---
+++ library_common/src/main/java/com/zspt/app/library_common/util/SnackBarUtil.java
@@ -0,0 +1,11 @@
+package com.zspt.app.library_common.util;
+
+/**
+ * Created by yuequan on 2017/10/9.
+ */
+
+public class SnackBarUtil   {
+
+    public static  void SnackBar(String title){
+    }
+}
--- module_main/src/main/java/com/zspt/app/modulemain/splash/SplashActivity.java
+++ module_main/src/main/java/com/zspt/app/modulemain/splash/SplashActivity.java
@@ -1,31 +1,23 @@
 package com.zspt.app.modulemain.splash;
 
 import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
-import com.zspt.app.library_common.base.activity.BaseActivity;
 import com.zspt.app.modulemain.MainActivity;
 
 /**
  * Created by yuequan on 2017/10/8.
  */
 
-public class SplashActivity extends BaseActivity {
+public class SplashActivity extends AppCompatActivity {
+
 
     @Override
-    protected int bindLayoutId() {
-        return 0;
-    }
-
-    @Override
-    protected void initView() {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         startActivity(new Intent(this, MainActivity.class));
         finish();
     }
-
-    @Override
-    protected void initData() {
-
-    }
-
-
 }
